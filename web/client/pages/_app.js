4aef99de18a80d050d349e849c5d769f7fc657f1
osawa-koki <github@osawa.dev>
2023-01-20 22:25:22 +0900

    スタイルシート。

--- web/client/pages/_app.js
+++ web/client/pages/_app.js
@@ -5,6 +5,8 @@ import '../styles/styles.scss';
 import '../styles/index.scss';
 import '../styles/about.scss';
 import '../styles/contact.scss';
+import '../styles/info.scss';
+import '../styles/note.scss';
 
 import Head from 'next/head';
 
